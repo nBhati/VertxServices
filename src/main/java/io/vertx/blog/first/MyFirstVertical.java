b788177717a0fdb322372dda8e243aa364d0c312
nBhati <narayansinghbhati@gmail.com>
2019-02-23 20:46:32 -0700

    Rest Services and UI added

--- src/main/java/io/vertx/blog/first/MyFirstVertical.java
+++ src/main/java/io/vertx/blog/first/MyFirstVertical.java
@@ -1,26 +1,122 @@
 package io.vertx.blog.first;
 
+import io.netty.handler.codec.http.HttpResponse;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created by nBhati on 2/23/19.
  */
 public class MyFirstVertical extends AbstractVerticle {
-    @Override
-    public void start(Future<Void> fut){
-        vertx.createHttpServer()
-                .requestHandler(r->{
-                    r.response().end("<h1>Hello from my first " +
-                            "Vert.x 3 application</h1>");
-                })
-                .listen(8080,result->{
-                    if(result.succeeded()){
-                        fut.complete();
-                    }else{
-                        fut.fail(result.cause());
-                    }
-                });
+    // Store our product
+    private Map<Integer, Whisky> products = new LinkedHashMap<>();
+    // Create some product
+    private void createSomeData() {
+        Whisky bowmore = new Whisky("Bowmore 15 Years Laimrig", "Scotland, Islay");
+        products.put(bowmore.getId(), bowmore);
+        Whisky talisker = new Whisky("Talisker 57° North", "Scotland, Island");
+        products.put(talisker.getId(), talisker);
+    }
 
+    @Override
+    public void start(Future<Void> fut) {
+        this.createSomeData();
+
+        Router router=Router.router(vertx);
+
+        router.route("/").handler(routingContext -> {
+            HttpServerResponse response=routingContext.response();
+            response.putHeader("Content-type","text/html")
+                    .end("<h1>Hello from my first Vert.x 3 application to you</h1>");
+        });
+
+        // Serve static resources from the /assets directory
+        router.route("/assets/*").handler(StaticHandler.create("assets"));
+
+        router.get("/api/whiskies").handler(this::getAll);
+        router.route("/api/whiskies*").handler(BodyHandler.create());
+        router.post("/api/whiskies").handler(this::addOne);
+        router.delete("/api/whiskies/:id").handler(this::deleteOne);
+        router.get("/api/whiskies/:id").handler(this::getOne);
+        router.put("/api/whiskies/:id").handler(this::updateOne);
+
+        vertx.createHttpServer()
+                .requestHandler(router::accept)
+                .listen(
+                        config().getInteger("http.port", 8080),
+                        result -> {
+                            if (result.succeeded()) {
+                                fut.complete();
+                            } else {
+                                fut.fail(result.cause());
+                            }
+                        });
+
+    }
+    private void getAll(RoutingContext routingContext) {
+        routingContext.response()
+                .putHeader("content-type", "application/json; charset=utf-8")
+                .end(Json.encodePrettily(products.values()));
+    }
+
+    private void addOne(RoutingContext routingContext) {
+        final Whisky whisky = Json.decodeValue(routingContext.getBodyAsString(),
+                Whisky.class);
+        products.put(whisky.getId(), whisky);
+        routingContext.response()
+                .setStatusCode(201)
+                .putHeader("content-type", "application/json; charset=utf-8")
+                .end(Json.encodePrettily(whisky));
+    }
+
+    private void deleteOne(RoutingContext routingContext) {
+        String id = routingContext.request().getParam("id");
+        if (id == null) {
+            routingContext.response().setStatusCode(400).end();
+        } else {
+            Integer idAsInteger = Integer.valueOf(id);
+            products.remove(idAsInteger);
+        }
+        routingContext.response().setStatusCode(204).end();
+    }
+
+    private void getOne(RoutingContext routingContext){
+        String id=routingContext.request().getParam("id");
+        if(id==null){
+            routingContext.response().setStatusCode(400).end();
+        }else{
+            Integer idAsInteger = Integer.valueOf(id);
+            routingContext.response()
+                    .putHeader("content-type", "application/json; charset=utf-8")
+                    .end(Json.encodePrettily(products.get(idAsInteger)));
+        }
+    }
+
+    private void updateOne(RoutingContext routingContext){
+        String id=routingContext.request().getParam("id");
+        JsonObject object=routingContext.getBodyAsJson();
+        System.out.println(id+" "+object);
+        if(id==null){
+            routingContext.response().setStatusCode(400).end();
+        }else{
+            Integer idAsInteger = Integer.valueOf(id);
+            Whisky  whisky= products.get(idAsInteger);
+            whisky.setName(object.getString("name"));
+            whisky.setOrigin(object.getString("origin"));
+            routingContext.response()
+                    .putHeader("content-type", "application/json; charset=utf-8")
+                    .setStatusCode(200)
+                    .end(Json.encodePrettily(whisky));
+        }
     }
 }
--- src/test/java/io/vertx/blog/first/MyFirstVerticalTest.java
+++ src/test/java/io/vertx/blog/first/MyFirstVerticalTest.java
@@ -1,6 +1,8 @@
 package io.vertx.blog.first;
 
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
@@ -8,6 +10,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.net.ServerSocket;
 
 /**
  * Created by nBhati on 2/23/19.
@@ -17,23 +22,23 @@ public class MyFirstVerticalTest {
     private Vertx vertx;
 
     @Before
-    public void setUp(TestContext context){
-        vertx=Vertx.vertx();
+    public void setUp(TestContext context) throws IOException {
+        vertx = Vertx.vertx();
         vertx.deployVerticle(MyFirstVertical.class.getName(),
                 context.asyncAssertSuccess());
     }
 
     @After
-    public void tearDown(TestContext context){
+    public void tearDown(TestContext context) {
         vertx.close(context.asyncAssertSuccess());
     }
 
     @Test
-    public void testMyApplication(TestContext context){
-        final Async async=context.async();
-        vertx.createHttpClient().getNow(8080,"localhost","/",
-                response ->{
-                    response.handler(body->{
+    public void testMyApplication(TestContext context) {
+        final Async async = context.async();
+        vertx.createHttpClient().getNow(8080,"localhost", "/",
+                response -> {
+                    response.handler(body -> {
                         context.assertTrue(body.toString().contains("Hello"));
                         async.complete();
                     });
